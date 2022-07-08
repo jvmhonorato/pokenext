7b30a6e293bd7b2da4dfefeb5e5d8c85f96bcd53
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-07-08 13:35:15 -0300

    feita a chamada da API

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,38 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  //buscar dados na API
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  //criar um await com o limit máximos de dados a srem buscados
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  //transformar dados em json()
+  const data = await res.json()
+
+  // add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+  return {
+    props: {
+      pokemons: data.results,
+    }
+  }
+
+}
+
+export default function Home({pokemons}) {
   return (
     <div>
      <h1>PokeNext</h1>
+     <ul>
+      {pokemons.map((pokemon)=>(
+        <li key={pokemon.id}>{pokemon.name}</li>
+      ))}
+
+     </ul>
     </div>
   )
 }
