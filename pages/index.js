73d3df12e43f640091bdad773502c296e286a1aa
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-07-08 16:21:57 -0300

    Finaliazado a página Home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Card from './components/Card'
+
 
 export async function getStaticProps() {
   //buscar dados na API
@@ -26,14 +28,23 @@ export async function getStaticProps() {
 
 export default function Home({pokemons}) {
   return (
-    <div>
-     <h1>PokeNext</h1>
-     <ul>
-      {pokemons.map((pokemon)=>(
-        <li key={pokemon.id}>{pokemon.name}</li>
-      ))}
+      <>
+      <div className={styles.title_container} >
+       <h1 className={styles.title}>Poke<span>Next</span></h1>
+       <Image 
+       src='/images/pokeball.png' 
+       width="50" 
+       height="50" 
+       alt="PokeNext"
+       />
+      </div>
+       <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon)=>(
+        <Card key={pokemon.id} pokemon={pokemon}/>
+        ))}
 
-     </ul>
-    </div>
+       </div>
+      
+     </>
   )
 }
