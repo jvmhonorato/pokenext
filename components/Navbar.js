5174a7e43295a615290046d12f6369475c390c8e
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-07-08 19:20:39 -0300

    Rotas reorganizadas

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../../styles/Navbar.module.css'
+import styles from '../styles/Navbar.module.css'
 
 
 export default function Navbar(){
